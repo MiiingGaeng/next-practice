6a4e117d556445a3fe0538e49825bb064651f325
miing_gaeng <pem4134@naver.com>
2025-03-14 12:07:28 +0900

    3단계 완료

--- src/types/type.ts
+++ src/types/type.ts
@@ -3,3 +3,12 @@ export type introData = {
   description2: string;
   description3: string;
 };
+
+export type newsItem = {
+  id: string;
+  title: string;
+  content: string;
+  author: string;
+  category: string;
+  createdAt: number;
+};
