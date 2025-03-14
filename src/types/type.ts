f836e4bc44d471ff6518374b6afff6243211d5df
miing_gaeng <pem4134@naver.com>
2025-03-14 12:28:35 +0900

    4단계 해결

--- src/types/type.ts
+++ src/types/type.ts
@@ -12,3 +12,13 @@ export type newsItem = {
   category: string;
   createdAt: number;
 };
+
+export type blogItem = {
+  id: string;
+  title: string;
+  content: string;
+  author: string;
+  category: string;
+  createdAt: number;
+  tags: string[];
+};
