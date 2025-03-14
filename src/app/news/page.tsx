65eb316dbc62443d6cc9ea5a918f072193c9abe8
miing_gaeng <pem4134@naver.com>
2025-03-14 11:38:51 +0900

    1단계 성공

---
+++ src/app/news/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const newsPage = () => {
+  return <div>newsPage</div>;
+};
+
+export default newsPage;
---
+++ src/app/todo-list/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const todoPage = () => {
+  return <div>todoPage</div>;
+};
+
+export default todoPage;
